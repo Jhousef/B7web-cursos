e8b289d7dd4f54aba99dc794491a95e11aad09f4
jhousef <jhousefkl@hotmail.com>
2025-01-08 22:56:45 -0300

    conta bancaria com cheque especial

--- php-poo/modulo-5/conta.php
+++ php-poo/modulo-5/conta.php
@@ -2,16 +2,22 @@
 
 class Conta {
   
-  public function __construct(private int $saldo)
+  public function __construct(private int $saldo, private bool $chequeEspecial = false)
   {
     
   }
-
+  
   public function sacar(int $valor)
   {
 
-    if($this->saldo < $valor) {
-      return 'O valor a sacar é maior que o saldo atual! <br>';
+    if($valor > $this->saldo && $this->chequeEspecial == false) {
+      echo 'O valor a sacar é maior que o saldo atual! <br>';
+      return false;
+    }
+
+    if($valor > $this->saldo + 100 && $this->chequeEspecial = true) {
+      echo 'Você não pode sacar mais do que o valor do saldo + cheque especial (100) <br>';
+      return false;
     }
 
     echo 'Você sacou: '.$valor . '<br>';
@@ -22,7 +28,8 @@ class Conta {
   {
 
     if($valor>100) {
-      return 'deposito maior que R$ 100, não permitido! <br>';
+      echo 'deposito maior que R$ 100, não permitido! <br>';
+      return false;
     }
 
     echo 'Você depositou: ' . $valor . '<br>';
@@ -36,12 +43,18 @@ class Conta {
 
 }
 
-$conta1 = new Conta(50);
-echo $conta1->depositar(100);
-echo $conta1->depositar(50);
-echo $conta1->depositar(100);
-echo $conta1->depositar(80);
-echo $conta1->depositar(90);
-echo $conta1->sacar(470);
+$conta1 = new Conta(500, false);
+echo 'Saldo inicial: ' . $conta1->verSaldo() . '<br>';
+$conta1->depositar(100);
+$conta1->sacar(700);
 
 echo 'Saldo atual: ' . $conta1->verSaldo() . '<br>';
+
+echo '<hr>';
+$conta2 = new Conta(500, true);
+echo 'Saldo inicial: ' . $conta2->verSaldo() . '<br>';
+$conta2->depositar(100);
+$conta2->sacar(700);
+// var_dump($conta2);
+
+echo 'Saldo atual: ' . $conta2->verSaldo() . '<br>';
