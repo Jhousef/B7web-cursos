f6ba718410b25798a921c394bf69bc70a5fc0407
jhousef <jhousefkl@hotmail.com>
2025-01-13 20:30:40 -0300

    Exercicio Prático - Classes Abstratas

--- php-poo/modulo-5/classesAbstratas.php
+++ php-poo/modulo-5/classesAbstratas.php
@@ -2,8 +2,8 @@
 
 abstract class CalculadoraDeAreas
 {
-  public function __construct(public int $lado, public int $altura, public int $base)
-  {}
+  // public function __construct(public int $lado, public int $altura, public int $base)
+  // {}
 
   abstract public function realizarCalculo();
 
@@ -11,35 +11,44 @@ abstract class CalculadoraDeAreas
 
 class Quadrado extends CalculadoraDeAreas
 {
-  public function __construct($lado, $altura, $base)
-  {
-    parent::__construct($lado, $altura, $base);
-  }
+  public function __construct(public int $lado)
+  {}
 
   public function realizarCalculo()
   {
-
+    return $this->lado * $this->lado;
   }
 
 }
 
 class Retangulo extends CalculadoraDeAreas
 {
+  public function __construct(public int $base, public int $altura)
+  {}
+
   public function realizarCalculo()
   {
-    
+    return $this->base *  $this->altura;
   }
 }
 
 class Triangulo extends CalculadoraDeAreas
 {
+
+  public function __construct(public int $base, public int $altura)
+  {}
+
   public function realizarCalculo()
   {
-    
+    return $this->base * $this->altura / 2;
   }
 }
 
-function CalculadoraDeArea()
-{
-  
-}
+$quadrado = new Quadrado(10);
+echo 'A área do quadrado é: ' . $quadrado->realizarCalculo() . '<br>';
+
+$retangulo = new Retangulo(10, 2);
+echo 'A área do retangulo é: ' . $retangulo->realizarCalculo() . '<br>';
+
+$triangulo = new Triangulo(10 ,2);
+echo 'A área do triangulo é: ' . $triangulo->realizarCalculo() . '<br>';
