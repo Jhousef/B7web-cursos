d07f854c68aa2cb45370323c288de005f306fdd3
jhousef <jhousefkl@hotmail.com>
2025-01-15 21:42:18 -0300

    Interfaces na Prática [2/2]

--- php-poo/modulo-5/interfaces.php
+++ php-poo/modulo-5/interfaces.php
@@ -1,25 +1,50 @@
 <?php
 
-class Paypal
+interface MetodoPagamento {
+  public function pagar(float $valor):bool;
+}
+
+class Paypal implements MetodoPagamento
 {
-  public function pagar($valor)
+  public function pagar(float $valor):bool
   {
-    echo 'Pagamento via Paypal no valor de R$ ' .$valor;
+    echo 'Pagamento via Paypal no valor de R$ ' .$valor . '<br>';
     return true;
   }
 }
 
-class CreditCard
+class CreditCard implements MetodoPagamento
 {
-  public function pagar($valor)
+  public function pagar(float $valor):bool
   {
-    echo 'Pagamento via cartão de credito no valor de R$ ' .$valor;
+    echo 'Pagamento via cartão de credito no valor de R$ ' .$valor . '<br>';
+    return true;
+  }
+}
+
+class Pix implements MetodoPagamento
+{
+  private function gerarQrCode(float $valor): string
+  {
+    echo 'Gerando QR Code para pagamento via Pix no valor de R$ ' .$valor . '<br>';
+    return 'QRCODE';
+  }
+  public function pagar(float $valor):bool
+  {
+    $qrCode = $this->gerarQrCode($valor);
+    echo 'Pagamento via Pix no valor de R$ ' .$valor . '<br>';
+    echo 'QR Code: ' .$qrCode . '<br>';
+    return true;
   }
 }
 
 // Nosso sistema -> imutavel
-$pagamento = new CreditCard();
-$pagamento->pagar(100);
+$pagamento = new Pix();
+if ($pagamento->pagar(100) === true) {
+  echo 'Aprovado...';
+} else {
+  echo 'Rejeitado...';
+}
 
 
 // Nosso sistema usando Interface
