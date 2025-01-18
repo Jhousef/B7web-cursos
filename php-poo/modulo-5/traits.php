1152b1fb4393525556204b8b431549ca96f0f1ce
jhousef <jhousefkl@hotmail.com>
2025-01-17 23:46:26 -0300

    correcao funcao strlower no diretorio e classeName no foreach

--- php-poo/modulo-5/traits.php
+++ php-poo/modulo-5/traits.php
@@ -4,8 +4,12 @@ function meu_autoloader($className) {
   $diretorios = ['classes/', 'traits/'];
 
   foreach ($diretorios as $diretorio){
-    if(file_exists($diretorio . strTolower($className) . '.class.php')){
-      include_once($diretorio . strTolower($className) . '.class.php');
+
+    $diretorio = strtolower($diretorio);
+    $className = strtolower($className);
+
+    if(file_exists($diretorio . $className . '.class.php')){
+      include_once($diretorio . $className . '.class.php');
       break;
     }
   }
